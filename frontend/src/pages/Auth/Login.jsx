82509ff0339a131166d9e6c9a9fa0c2bb3b53753
SumitGarg <garg10sumit@gmail.com>
2025-06-23 19:47:42 +0530

    add reusable Input component with password toggle feature

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
 
 const Login = () => {
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [error, setError] = useState(null);
-    const naviagte = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const naviagte = useNavigate();
+
+  // handleLogin
+
+  const handleLogin = async (e) => {};
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center ">
@@ -14,6 +19,15 @@ const Login = () => {
         <p className="text-xs text-slate-700 mt-[5px] mb-6 ">
           Please enter your details to log in
         </p>
+        <form onSubmit={handleLogin}>
+          <Input
+            type="text"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+          />
+        </form>
       </div>
     </AuthLayout>
   );
