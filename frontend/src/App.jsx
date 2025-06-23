020ca4c422acdae6cf67628dfc405d5ae4710e2a
SumitGarg <garg10sumit@gmail.com>
2025-06-23 18:11:11 +0530

    add root function for client-side auth check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Route, Router, Routes } from "react-router-dom";
+import { Navigate, Route, Router, Routes } from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
 
 const App = () => {
   return <div className=""> 
@@ -10,9 +13,21 @@ const App = () => {
       <Route path="/" element={<Root />} />
       <Route path="/login" exact element={<Login/>} />
       <Route path="/signUp" exact  element={<SignUp/>} />
+      <Route path="/dashboard" exact  element={<Home/>} />
+      <Route path="/income" exact  element={<Income/>} />
+      <Route path="/expense" exact  element={<Expense/>} />
     </Routes>
   </Router>
   </div>;
 };
 
 export default App;
+
+const Root = () => {
+  // check if token exits in localStorage
+
+  const isAuthenticated = !!localStorage.getItem("token");
+  // redirect to dashboard if authenticated, otherwise to login 
+
+  return isAuthenticated ? (<Navigate to="/dashboard" />) : ( <Navigate to="/login" /> )
+}
