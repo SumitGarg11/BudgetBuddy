99209b83776a6d969056086b529eb785c7619100
SumitGarg <garg10sumit@gmail.com>
2025-06-23 17:56:44 +0530

    add routes for login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, Router, Routes } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
 
 const App = () => {
-  return <div className="text-rose-600"> App</div>;
+  return <div className=""> 
+  <Router>
+    <Routes>
+      <Route path="/" element={<Root />} />
+      <Route path="/login" exact element={<Login/>} />
+      <Route path="/signUp" exact  element={<SignUp/>} />
+    </Routes>
+  </Router>
+  </div>;
 };
 
 export default App;
