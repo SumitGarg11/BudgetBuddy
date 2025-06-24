38bbf41097cfb0810525f5cb518a5914c87d2ef3
SumitGarg <garg10sumit@gmail.com>
2025-06-24 10:42:05 +0530

    setup auth routes with register, login, and getUser endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express')
 const cors = require('cors');
-const path = require('path')
+const path = require('path');
+const connectDB = require('./config/db');
 
 const app = express();
 app.use(
@@ -13,5 +14,10 @@ app.use(
     })
 );
 app.use((express.json()));
+
+connectDB();
+app.use("/api/v1/auth",authRoutes);
+
 const PORT= process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log("Server running on port  ",PORT));
